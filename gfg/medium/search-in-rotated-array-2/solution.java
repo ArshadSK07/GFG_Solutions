67b4df91d8150f52c7b6c75ecb31817a768ad29e
Shaik kanekal Arshad <arshadkanekal@gmail.com>
2026-09-08 07:25:11 +0530

    ✅ Search in Rotated Array 2 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-08

--- gfg/medium/search-in-rotated-array-2/solution.java
+++ gfg/medium/search-in-rotated-array-2/solution.java
@@ -1,55 +1,55 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
+    public boolean search(int[] arr, int key) {
         // code here
-        int low=0;
-                int high=nums.length-1;
-                while(low<=high){
-                    int mid=low+(high-low)/2;
-                    if(nums[mid]==target)  
-                        return true;
-                    if(nums[low] == nums[mid] && nums[mid] == nums[high]){
-                        low+=1;
-                        high-=1;
-                        continue;
-                    }
-                    if(nums[low] <= nums[mid]){
-                        if(target >=nums[low] && target <=nums[mid])
-                            high=mid-1;
-                        else
-                            low=mid+1;
-                    }else{
-                        if(target >= nums[mid] && target <= nums[high])
-                            low=mid+1;
-                        else
-                            high=mid-1;
-                    }
-                }
-
-                return false;
-
         // int low=0;
-        // int high=arr.length-1;
-        // while(low<=high){
-        //     int mid=low-(high-low)/2;
-        //     if(arr[mid]==key) return true;
-        //     if(arr[low]==arr[mid] && arr[mid]==arr[high]) {
-        //         low+=1;
-        //         high-=1;
-                    // continue;
-        //     }
-        //     if(arr[low]<=arr[mid]){
-        //         if(key >= arr[low] && key<=arr[mid])
-        //             high=mid-1;
-        //         else
-        //             low=mid+1;
-        //     }else{
-        //         if(key>=arr[mid] && key <=arr[high])
-        //             low=mid+1;
-        //         else{
-        //             high=mid-1;
+        //         int high=nums.length-1;
+        //         while(low<=high){
+        //             int mid=low+(high-low)/2;
+        //             if(nums[mid]==target)  
+        //                 return true;
+        //             if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+        //                 low+=1;
+        //                 high-=1;
+        //                 continue;
+        //             }
+        //             if(nums[low] <= nums[mid]){
+        //                 if(target >=nums[low] && target <=nums[mid])
+        //                     high=mid-1;
+        //                 else
+        //                     low=mid+1;
+        //             }else{
+        //                 if(target >= nums[mid] && target <= nums[high])
+        //                     low=mid+1;
+        //                 else
+        //                     high=mid-1;
+        //             }
         //         }
-        //     }
-        // }
-        // return false;
+
+        //         return false;
+
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==key) return true;
+            if(arr[low]==arr[mid] && arr[mid]==arr[high]) {
+                low+=1;
+                high-=1;
+                continue;
+            }
+            if(arr[low]<=arr[mid]){
+                if(key >= arr[low] && key<=arr[mid])
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }else{
+                if(key>=arr[mid] && key <=arr[high])
+                    low=mid+1;
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+        return false;
     }
 }
